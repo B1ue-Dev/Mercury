c21ab9045e74e09353acc2d0889e9139f22fdef2
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-05-15 15:07:09 +0700

    chore: root bot file

---
+++ bot.py
@@ -0,0 +1,31 @@
+import interactions
+import os
+from dotenv import load_dotenv
+load_dotenv()
+bot_token = os.getenv('TOKEN')
+
+
+
+bot = interactions.Client(bot_token,
+	intents=interactions.Intents.ALL,
+	#disable_sync=True
+)
+
+
+bot.load('exts.mod')
+bot.load('exts.modmail')
+
+
+
+@bot.event
+async def on_ready():
+	websocket = f"{bot.latency * 1:.0f}"
+	print('Ready!')
+	print(f'Latency: {websocket}ms')
+
+
+
+
+
+
+bot.start()
