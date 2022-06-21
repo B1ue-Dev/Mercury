ba4ee2d8e9164de27248168e97f406e30e61a17d
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-06-21 16:46:57 +0700

    feat: Adding about command

--- src/bot.py
+++ src/bot.py
@@ -6,7 +6,7 @@ Root bot file.
 
 import os
 import interactions
-from const import TOKEN
+from const import TOKEN, SCOPE
 
 client = interactions.Client(
     token=TOKEN,
@@ -29,5 +29,36 @@ async def on_ready():
     websocket = f"{client.latency * 1:.0f}"
     print('Bot is ready.')
     print(f'Latency: {websocket}ms')
+
+@client.command(
+    name="about",
+    description="About Mercury",
+    scope=SCOPE,
+)
+async def _about(ctx: interactions.CommandContext):
+    embed = interactions.Embed(
+        title="Mercury",
+        description="Mercury is a modmail Discord bot that members can use to get in touch with the moderation team.\n\nThis project is created and maintained by Blue#2095. You can have a look at the repository and his profile by clicking on the buttons below.",
+        color=0x08a46c,
+        footer=interactions.EmbedFooter(
+            text=f"Requested by {ctx.user.username}#{ctx.user.discriminator}",
+            icon_url=ctx.user.avatar_url
+        )
+    )
+
+    buttons = [
+        interactions.Button(
+            style=interactions.ButtonStyle.LINK,
+            label="Repository",
+            url="https://github.com/Jimmy-Blue/Mercury"
+        ),
+        interactions.Button(
+            style=interactions.ButtonStyle.LINK,
+            label="Profile",
+            url="https://blue.is-a.dev"
+        )
+    ]
+
+    await ctx.send(embeds=embed, components=buttons)
 
 client.start()
