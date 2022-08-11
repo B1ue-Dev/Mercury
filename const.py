3f40586607f6f4d2b7b77b0da68e1d8af9f07177
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-08-11 22:57:01 +0700

    feat: Load Extension within one line

--- const.py
+++ const.py
@@ -11,7 +11,9 @@ dotenv.load_dotenv()
 global TOKEN
 global MODMAIL_CHANNEL
 global SCOPE
+global EXTENSION
 
 TOKEN = os.getenv('TOKEN')
 MODMAIL_CHANNEL = os.getenv('MODMAIL_CHANNEL')
 SCOPE = int(os.getenv('SCOPE'))
+EXTENSION = [file.replace(".py", "") for file in os.listdir("exts") if not file.startswith("_")]
