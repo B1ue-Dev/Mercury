fefcc112bab29813afdceacbfbe71578ed666d15
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-08-18 00:58:32 +0700

    refactor: Using the new .subcommand()

--- exts/mod.py
+++ exts/mod.py
@@ -5,149 +5,142 @@ Moderation commands.
 """
 
 import json
-import interactions
+from interactions import (
+    extension_command,
+    option,
+    Extension,
+    Client,
+    CommandContext,
+    OptionType,
+    Permissions,
+    Member,
+    Embed,
+    EmbedImageStruct,
+    EmbedField,
+)
 from const import SCOPE
 
 
-class Mod(interactions.Extension):
-    def __init__(self, client: interactions.Client):
-        self.client: interactions.Client = client
+class Mod(Extension):
+    """Moderation commands for Mercury."""
 
-    @interactions.extension_command(
-        name="modmail",
-        description="Moderation command for the bot",
+    def __init__(self, client: Client) -> None:
+        self.client: Client = client
+
+    @extension_command(
         scope=SCOPE,
-        options=[
-            interactions.Option(
-                type=interactions.OptionType.SUB_COMMAND,
-                name="block",
-                description="Block a user from using the bot",
-                options=[
-                    interactions.Option(
-                        type=interactions.OptionType.USER,
-                        name="user",
-                        description="The user to block",
-                        required=True
-                    ),
-                    interactions.Option(
-                        type=interactions.OptionType.STRING,
-                        name="reason",
-                        description="The reason for blocking the user",
-                        required=True
-                    ),
-                ],
-            ),
-            interactions.Option(
-                type=interactions.OptionType.SUB_COMMAND,
-                name="unblock",
-                description="Unblock a user from using the bot",
-                options=[
-                    interactions.Option(
-                        type=interactions.OptionType.USER,
-                        name="user",
-                        description="The user to unblock",
-                        required=True
-                    ),
-                ],
-            ),
-            interactions.Option(
-                type=interactions.OptionType.SUB_COMMAND,
-                name="list",
-                description="List blocked users",
-            ),
-            interactions.Option(
-                type=interactions.OptionType.SUB_COMMAND,
-                name="check",
-                description="Check if a user is blocked",
-                options=[
-                    interactions.Option(
-                        type=interactions.OptionType.USER,
-                        name="user",
-                        description="The user to check",
-                        required=True
-                    )
+        default_member_permissions=Permissions.KICK_MEMBERS,
+    )
+    async def modmail(self, *args, **kwargs):
+        ...
+
+    @modmail.subcommand()
+    @option(
+        description="The user you wish to block.",
+        type=OptionType.USER,
+        required=True,
+    )
+    @option(
+        description="The reason for the action.",
+        required=False,
+    )
+    async def block(self, ctx: CommandContext, user: Member, reason: str = "N/A"):
+        """Block a user from using Mercury."""
+
+        user_id = str(user.user.id)
+
+        _blocked = json.loads(open("./db/blocked.json", "r").read())
+
+        if user_id in _blocked:
+            return await ctx.send(f"{user.mention} is already blocked.", ephemeral=True)
+
+        _blocked[user_id] = {
+            "reason": reason,
+            "blocked_by": str(ctx.author.id)
+        }
+
+        with open("./db/blocked.json", "w") as f:
+            json.dump(_blocked, f, indent=4)
+
+        await ctx.send(f"{user.mention} was blocked.")
+
+    @modmail.subcommand()
+    @option(
+        description="The user you wish to unblock.",
+    )
+    async def unblock(self, ctx: CommandContext, user: Member):
+        """Unblock a user from using Mercury."""
+
+        user_id = str(user.user.id)
+
+        _blocked = json.loads(open("./db/blocked.json", "r").read())
+
+        if user_id not in _blocked:
+            return await ctx.send(f"{user.mention} was not blocked.", ephemeral=True)
+
+        del _blocked[user_id]
+
+        with open("./db/blocked.json", "w") as f:
+            json.dump(_blocked, f, indent=4)
+
+        await ctx.send(f"{user.mention} was unblocked.")
+
+    @modmail.subcommand()
+    async def list(self, ctx: CommandContext):
+        """List all blocked users."""
+
+        _blocked = json.loads(open("./db/blocked.json", "r").read())
+
+        if len(_blocked) == 0:
+            return await ctx.send("No blocked user.", ephemeral=True)
+
+        embed = Embed(
+            title="".join("Blocked users:" if len(_blocked) > 1 else "Block user:"),
+            description="\n".join(
+                [
+                    f"<@{user_id}>: {_blocked[user_id]['reason']}"
+                    for user_id in _blocked
                 ]
             )
-        ],
-        default_member_permissions=interactions.Permissions.KICK_MEMBERS,
-        dm_permission=False,
-    )
-    async def _modmail(self, ctx: interactions.CommandContext,
-        sub_command: str,
-        user: interactions.Member = None,
-        reason: str = None
-    ):
-        if sub_command == "block":
-            await self._modmail_block(ctx, user, reason)
-        elif sub_command == "unblock":
-            await self._modmail_unblock(ctx, user)
-        elif sub_command == "list":
-            await self._modmail_list(ctx)
-        elif sub_command == "check":
-            await self._modmail_check(ctx, user)
+        )
 
+        await ctx.send(embeds=embed)
 
-    async def _modmail_block(self, ctx: interactions.CommandContext, user: interactions.Member, reason: str):
+    @modmail.subcommand()
+    async def check(self, ctx: CommandContext, user: Member):
+        """Check if a user is blocked from Mercury."""
+
         user_id = str(user.user.id)
-        _blocked_list = json.loads(open("./db/blocked.json", "r").read())
-        if str(user_id) in _blocked_list:
-            await ctx.send(f"{user.mention} is already blocked.", ephemeral=True)
-        else:
-            _blocked_list[user_id] = {
-                "reason": reason,
-                "blocked_by": str(ctx.author.id)
-            }
-            with open("./db/blocked.json", "w") as f:
-                json.dump(_blocked_list, f, indent=4)
-            await ctx.send(f"{user.mention} was blocked.")
 
+        _blocked = json.loads(open("./db/blocked.json", "r").read())
 
-    async def _modmail_unblock(self, ctx: interactions.CommandContext, user: interactions.Member):
-        user_id = str(user.user.id)
-        _blocked_list = json.loads(open("./db/blocked.json", "r").read())
-        if str(user_id) in _blocked_list:
-            del _blocked_list[user_id]
-            with open("./db/blocked.json", "w") as f:
-                json.dump(_blocked_list, f, indent=4)
-            await ctx.send(f"{user.mention} was unblocked.")
-        else:
-            await ctx.send(f"{user.mention} was not blocked.", ephemeral=True)
+        if user_id not in _blocked:
+            return await ctx.send(f"{user.mention} was not blocked.", ephemeral=True)
 
-
-    async def _modmail_list(self, ctx: interactions.CommandContext):
-        _blocked_list = json.loads(open("./db/blocked.json", "r").read())
-        if len(_blocked_list) == 0:
-            await ctx.send("No blocked user.", ephemeral=True)
-        else:
-            embed = interactions.Embed(
-                title="".join("Blocked users:" if len(_blocked_list) > 1 else "Block user:"),
-                description="\n".join([
-                    f"<@{user_id}>: {_blocked_list[user_id]['reason']}"
-                    for user_id in _blocked_list
-                ])
+        thumbnail = EmbedImageStruct(url=user.user.avatar_url)
+        fields = [
+            EmbedField(
+                name="Reason",
+                value=_blocked[user_id]['reason'],
+                inline=True,
+            ),
+            EmbedField(
+                name="Moderator",
+                value=f"<@{_blocked[user_id]['blocked_by']}>",
+                inline=True,
             )
-            await ctx.send(embeds=embed)
+        ]
+        embed = Embed(
+            title=f"{user.user.username}#{user.user.discriminator}",
+            description=f"<@{user.user.id}>",
+            thumbnail=thumbnail,
+            fields=fields,
+        )
+
+        await ctx.send(embeds=embed)
 
 
-    async def _modmail_check(self, ctx: interactions.CommandContext, user: interactions.Member):
-        user_id = str(user.user.id)
-        _blocked_list = json.loads(open("./db/blocked.json", "r").read())
-        if str(user_id) not in _blocked_list:
-            await ctx.send(f"{user.mention} was not blocked.", ephemeral=True)
-        else:
-            thumbnail = interactions.EmbedImageStruct(url=user.user.avatar_url)
-            fields = [
-                interactions.EmbedField(name="Reason", value=_blocked_list[user_id]['reason'], inline=True),
-                interactions.EmbedField(name="Moderator", value=f"<@{_blocked_list[user_id]['blocked_by']}>", inline=True)
-            ]
-            embed = interactions.Embed(
-                title=f"{user.user.username}#{user.user.discriminator}",
-                description=f"<@{user.user.id}>",
-                thumbnail=thumbnail,
-                fields=fields
-            )
-            await ctx.send(embeds=embed)
-
-
-def setup(client):
+def setup(client) -> None:
+    """Setup the Extension."""
     Mod(client)
+    print("Extension Mod loaded.")
